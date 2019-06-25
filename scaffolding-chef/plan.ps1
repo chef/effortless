a2ee9955db98c13edefdff7f01cddaa81dcf8bb0
echohack <echohack@users.noreply.github.com>
2019-06-25 11:51:20 -0700

    fix chef-dk dep so windows build will run
    Signed-off-by: echohack <echohack@users.noreply.github.com>

--- scaffolding-chef/plan.ps1
+++ scaffolding-chef/plan.ps1
@@ -6,7 +6,7 @@ $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=("Apache-2.0")
 $pkg_upstream_url="https://www.chef.sh"
 $pkg_build_deps=@(
-    "core/chefdk"
+    "core/chef-dk"
     "core/git"
   )
 function Invoke-Install {
