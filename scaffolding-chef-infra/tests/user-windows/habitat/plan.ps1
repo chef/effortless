070a6b85da10fd7a1d427f3007dbba13362cef26
David Echo <echohack@users.noreply.github.com>
2019-07-30 13:15:14 -0700

    Add license acceptance flow for Chef Infra
    Signed-off-by: David Echo <echohack@users.noreply.github.com>

--- scaffolding-chef-infra/tests/user-windows/habitat/plan.ps1
+++ scaffolding-chef-infra/tests/user-windows/habitat/plan.ps1
@@ -3,10 +3,9 @@
 # This tests a user interacting with the API provided by the scaffolding
 #######################################
 
-$pkg_name="user-windows
+$pkg_name="user-windows"
 $pkg_origin="ci"
-$pkg_version="0.1.0"
-$pkg_upstream_url="http://chef.io"
+$pkg_version="1.0.0"
 $pkg_build_deps=@("core/chef-dk")
 $pkg_deps=@(
   "core/cacerts"
@@ -15,4 +14,3 @@ $pkg_deps=@(
 $pkg_scaffolding="ci/scaffolding-chef-infra"
 $scaffold_policy_name="ci"
 $scaffold_cacerts="core/cacerts"
-scaffold_chef_client="chef/chef-client/14.13.11"
