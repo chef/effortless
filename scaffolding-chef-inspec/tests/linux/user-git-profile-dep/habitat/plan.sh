66231fb4a951a82c517651a82dea2e4af0c4a6c5
John Snow <thelunaticscripter@outlook.com>
2019-07-31 07:26:51 -0400

    Rename the test to follow infra pattern
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

---
+++ scaffolding-chef-inspec/tests/linux/user-git-profile-dep/habitat/plan.sh
@@ -0,0 +1,8 @@
+pkg_name=effortless-audit-baseline
+pkg_version=0.1.0
+pkg_origin=chef
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_description="The Audit Baseline InSpec Profile"
+pkg_upstream_url="http://chef.io"
+pkg_scaffolding="chef/scaffolding-chef-inspec"
