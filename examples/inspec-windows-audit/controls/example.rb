fe8e66e4cb4ee2a1e2798c02260fe7c6929a792c
Daniel Bright <danielcbright@gmail.com>
2019-08-02 13:36:26 -0400

    adding scaffold_report_on_install conditional to be able to report to Automate during Packer/other builds
    fixing default.toml examples for server_url and moving license acceptance to common location
    
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>
    
    Revert "fixing default.toml examples for server_url and moving license acceptance to common location"
    
    This reverts commit 9ca16b6b7be2f697b8eacbb21c03ba692707c81e.
    
    ran pre-commit, fixing issues
    
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>
    
    updating default.toml examples, fixes issue https://github.com/chef/effortless/issues/128
    
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>
    
    Revert "updating default.toml examples, fixes issue https://github.com/chef/effortless/issues/128"
    
    This reverts commit ae1e6360e57eb3531bc1fa8c9e65041dcd8254c2.

--- examples/inspec-windows-audit/controls/example.rb
+++ examples/inspec-windows-audit/controls/example.rb
@@ -3,7 +3,7 @@
 
 title 'Windows Baseline'
 
-include_controls 'windows-baseline' do 
+include_controls 'windows-baseline' do
     skip_control 'cis-network-access-2.2.2'
     skip_control 'windows-account-100'
     skip_control 'cis-act-as-os-2.2.3'
--- scaffolding-chef-inspec/tests/linux/user-git-profile-dep/controls/linux.rb
+++ scaffolding-chef-inspec/tests/linux/user-git-profile-dep/controls/linux.rb
@@ -6,7 +6,7 @@ title 'Linux Baseline'
 
 # TO DO - There is a know issue with Centos7 for ryslog and auditd package controls.
 # once the community cookbook has been updated we can add the skipped controls back
-include_controls 'linux-baseline' do 
+include_controls 'linux-baseline' do
   skip_control 'package-07'
   skip_control 'package-08'
   skip_control 'sysctl-33'
