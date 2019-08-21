e8603b50de4293dcbbd3544e2c7062236cdad6f6
John Snow <thelunaticscripter@outlook.com>
2019-08-21 10:46:53 -0400

    Fix all the errors from 139 and 126
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- scaffolding-chef-infra/lib/windows/client-chunk.rb
+++ scaffolding-chef-infra/lib/windows/client-chunk.rb
@@ -1,8 +1,8 @@
-cfg_env_path_prefix = {{cfg.env_path_prefix}}
+cfg_env_path_prefix = '{{cfg.env_path_prefix}}'
 cfg_env_path_prefix ||= ";C:/WINDOWS;C:/WINDOWS/system32/;C:/WINDOWS/system32/WindowsPowerShell/v1.0;C:/ProgramData/chocolatey/bin"
 ENV['PATH'] += cfg_env_path_prefix
 
-cfg_ssl_verify_mode = {{cfg.ssl_verify_mode}}
+cfg_ssl_verify_mode = '{{cfg.ssl_verify_mode}}'
 cfg_ssl_verify_mode ||= ":verify_peer"
 ssl_verify_mode "#{cfg_ssl_verify_mode}"
 
