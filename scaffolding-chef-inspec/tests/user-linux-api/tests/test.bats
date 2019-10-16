36888f2d48bb81655919157cbf9cd11b5f48c514
John Snow <thelunaticscripter@outlook.com>
2019-10-16 14:38:37 -0400

    Add tests for inspec client change
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

---
+++ scaffolding-chef-inspec/tests/user-linux-api/tests/test.bats
@@ -0,0 +1,22 @@
+SCAFFOLD_PKG_INSPEC_CLIENT_VERSION="$(echo "${scaffold_chef_client}" | cut -d/ -f3)"
+
+@test "API: scaffold_cacerts matches run hook core/cacerts" {
+  result="$(grep '^export SSL_CERT_FILE.*' /hab/svc/${TEST_PKG_NAME}/hooks/run | cut -d/ -f4-5)"
+  [ "${result}" = "${SCAFFOLD_PKG_INSPEC_CLIENT_VERSION}" ]
+}
+
+@test "API: scaffold_cacerts matches run hook core/cacerts" {
+  result="$(grep '^export SSL_CERT_DIR.*' /hab/svc/${TEST_PKG_NAME}/hooks/run | cut -d/ -f4-5)"
+  [ "${result}" = "core/cacerts" ]
+}
+
+@test "API: scaffold_inspec_client version matches whats in the plan" {
+  result="$(hab pkg exec ${TEST_PKG_IDENT} inspec -v | head -n 1 | awk '{print $2}')"
+  [ "${result}" = "core/cacerts" ]
+}
+
+teardown(){
+  if [ "${BATS_TEST_NUMBER}" -eq ${#BATS_TEST_NAMES[@]} ]; then
+    hab svc unload "${TEST_PKG_IDENT}" || true
+  fi
+}
---
+++ scaffolding-chef-inspec/tests/user-linux-api/tests/test.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#/ Usage: test.sh <pkg_ident>
+#/
+#/ Example: test.sh chef/scaffolding-chef-infra/1.2.0/20181108151533
+#/
+
+set -euo pipefail
+
+if [[ -z "${1:-}" ]]; then
+  grep '^#/' < "${0}" | cut -c4-
+	exit 1
+fi
+
+TEST_PKG_IDENT="${1}"
+export TEST_PKG_IDENT
+TEST_PKG_NAME="$(echo "${TEST_PKG_IDENT}" | cut -d/ -f2)"
+export TEST_PKG_NAME
+export scaffold_inspec_client
+
+source "$(dirname "${0}")/../habitat/plan.sh"
+export pkg_svc_path
+
+hab pkg install core/bats --binlink
+hab pkg install "${TEST_PKG_IDENT}"
+
+SUP_WAIT_SECONDS=1
+
+hab sup term
+sleep "${SUP_WAIT_SECONDS}"
+echo "--- :habicat: Starting the supervisor"
+hab sup run &
+
+echo "Waiting ${SUP_WAIT_SECONDS} seconds for hab sup to start..."
+sleep "${SUP_WAIT_SECONDS}"
+
+LOAD_WAIT_SECONDS=10
+hab svc load "${TEST_PKG_IDENT}"
+echo "Waiting ${LOAD_WAIT_SECONDS} seconds for ${TEST_PKG_IDENT} to start..."
+
+sleep "${LOAD_WAIT_SECONDS}"
+bats "$(dirname "${0}")/test.bats"
