a9ab6f4ec998c5d2ff625739c64e572a547ef25c
John Snow <thelunaticscripter@outlook.com>
2019-10-21 09:32:43 -0400

    Forgot to save
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- scaffolding-chef-inspec/tests/user-linux-waiver/tests/test.sh
+++ scaffolding-chef-inspec/tests/user-linux-waiver/tests/test.sh
@@ -36,22 +36,5 @@ LOAD_WAIT_SECONDS=10
 hab svc load "${TEST_PKG_IDENT}"
 echo "Waiting ${LOAD_WAIT_SECONDS} seconds for ${TEST_PKG_IDENT} to start..."
 
-function parse_yaml {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
-}
-
 sleep "${LOAD_WAIT_SECONDS}"
 bats "$(dirname "${0}")/test.bats"
