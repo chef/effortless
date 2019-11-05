f4bb7510fbaaebec1cc3b48834c7fd86bcc8d862
John Snow <thelunaticscripter@outlook.com>
2019-11-05 09:12:35 -0500

    Set example to current build
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- examples/infra-windows-hardening/habitat/plan.ps1
+++ examples/infra-windows-hardening/habitat/plan.ps1
@@ -5,7 +5,7 @@ $pkg_upstream_url="http://chef.io"
 $pkg_build_deps=@("core/chef-dk")
 $pkg_deps=@(
   "core/cacerts"
-  "chef/chef-infra-client" # https://github.com/habitat-sh/habitat/issues/6671
+  "chef/chef-infra-client/15.4.63/20191104181446" # https://github.com/habitat-sh/habitat/issues/6671
 )
 $pkg_scaffolding="chef/scaffolding-chef-infra"
 $scaffold_policy_name="base"
