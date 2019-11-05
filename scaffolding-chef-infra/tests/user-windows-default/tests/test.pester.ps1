7f8ee5552b22d7d67e05a58d75a67ba6adb9d649
John Snow <thelunaticscripter@outlook.com>
2019-11-05 09:51:52 -0500

    Fix read out on the test
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- scaffolding-chef-infra/tests/user-windows-default/tests/test.pester.ps1
+++ scaffolding-chef-infra/tests/user-windows-default/tests/test.pester.ps1
@@ -49,7 +49,7 @@ Describe "Chef client run doesn't fail" {
     }
 
     Context "API: scaffold_chef_client matches run hook chef/chef-infra-client" {
-        It "The chef-client should be Stuart Preston's chef-client" {
+        It "The chef-client should be the official chef-client" {
             $chef_client_pkg = Get-Content "C:\hab\svc\user-windows-default\hooks\run" | Select-String -Pattern '\w+/bin/chef-client.bat -z'
             $chef_client_pkg = $chef_client_pkg -split ' '
             $chef_client_pkg = $chef_client_pkg[2].split('\')
