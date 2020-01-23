426d26281f1de8baa42635d2eb8874fc109e2748
John Snow <thelunaticscripter@outlook.com>
2020-01-23 08:45:28 -0500

    Set scaffold back to official
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- scaffolding-chef-inspec/tests/windows/user-git-profile-dep/habitat/plan.ps1
+++ scaffolding-chef-inspec/tests/windows/user-git-profile-dep/habitat/plan.ps1
@@ -3,4 +3,4 @@ $pkg_origin="chef"
 $pkg_version="0.1.0"
 $pkg_build_deps=@("chef/inspec")
 $pkg_deps=@("chef/inspec")
-$pkg_scaffolding="thelunaticscripter/scaffolding-chef-inspec"
+$pkg_scaffolding="chef/scaffolding-chef-inspec"
