81f99cb2bceb377994dccff0c29b76f4c5857e2c
John Snow <thelunaticscripter@outlook.com>
2020-03-13 07:51:48 -0400

    fix policyname in plan
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- examples/effortless_config/chef_repo_pattern/habitat/plan.sh
+++ examples/effortless_config/chef_repo_pattern/habitat/plan.sh
@@ -6,7 +6,7 @@ pkg_license=("Apache-2.0")
 pkg_upstream_url="http://chef.io"
 pkg_scaffolding="chef/scaffolding-chef-infra"
 pkg_svc_user=("root")
-scaffold_policy_name="$policy_name"
+scaffold_policy_name="base"
 
 
 #######################################
