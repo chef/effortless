08fa7fd575ce6a9243969fd1056217bbaec14b9d
Gregory Sloan <gsloan@chef.io>
2020-03-27 13:41:00 -0700

    Fix boolean value assignment in window audit example
    Signed-off-by: Gregory Sloan <gsloan@chef.io>

--- examples/effortless_audit/windows/habitat/plan.ps1
+++ examples/effortless_audit/windows/habitat/plan.ps1
@@ -8,4 +8,4 @@ $pkg_scaffolding="chef/scaffolding-chef-inspec"
 $scaffold_automate_server_url = "$env:AUTOMATE_SERVER_URL" # Example: https://foo.bar.com
 $scaffold_automate_user = "$env:AUTOMATE_USER"             # Example: "admin"
 $scaffold_automate_token = "$env:AUTOMATE_TOKEN"           # Example: "DI0WVxInnyGnWKRlZBGizTXySgk="
-#$scaffold_compliance_insecure = true                      # set true to ignore SSL cert error when retrieving profile from A2
+#$scaffold_compliance_insecure = $TRUE                      # set true to ignore SSL cert error when retrieving profile from A2
