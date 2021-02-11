6148a41ac817286c1712bbe4ab8acf4bc5b17838
gscho <greg.c.schofield@gmail.com>
2021-02-11 12:47:08 -0500

    Add support for attribute persistence settings
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- scaffolding-chef-infra/lib/linux/client-chunk.rb
+++ scaffolding-chef-infra/lib/linux/client-chunk.rb
@@ -19,3 +19,39 @@ chef_guid '{{sys.member_id}}'
 data_collector.token '{{cfg.automate.token}}'
 data_collector.server_url '{{cfg.automate.server_url}}'
 {{/if ~}}
+
+{{# if cfg.blocked_automatic_attributes ~}}
+blocked_automatic_attributes {{cfg.blocked_automatic_attributes}}
+{{/if ~}}
+
+{{# if cfg.blocked_default_attributes ~}}
+blocked_default_attributes {{cfg.blocked_default_attributes}}
+{{/if ~}}
+
+{{# if cfg.blocked_normal_attributes ~}}
+blocked_normal_attributes {{cfg.blocked_normal_attributes}}
+{{/if ~}}
+
+{{# if cfg.blocked_override_attributes ~}}
+blocked_override_attributes {{cfg.blocked_override_attributes}}
+{{/if ~}}
+
+{{# if cfg.allowed_automatic_attributes ~}}
+allowed_automatic_attributes {{cfg.allowed_automatic_attributes}}
+{{/if ~}}
+
+{{# if cfg.blocked_automatic_attributes ~}}
+blocked_automatic_attributes {{cfg.blocked_automatic_attributes}}
+{{/if ~}}
+
+{{# if cfg.allowed_default_attributes ~}}
+allowed_default_attributes {{cfg.allowed_default_attributes}}
+{{/if ~}}
+
+{{# if cfg.allowed_normal_attributes ~}}
+allowed_normal_attributes {{cfg.allowed_normal_attributes}}
+{{/if ~}}
+
+{{# if cfg.allowed_override_attributes ~}}
+allowed_override_attributes {{cfg.allowed_override_attributes}}
+{{/if ~}}
