6c62208f70986247157370ac705d462c59a75c7b
Collin McNeese <cmcneese@chef.io>
2021-11-19 08:43:54 -0600

    updates inspec user-*-api tests to 4.50.3
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- scaffolding-chef-inspec/tests/user-linux-api/habitat/plan.sh
+++ scaffolding-chef-inspec/tests/user-linux-api/habitat/plan.sh
@@ -2,4 +2,4 @@ pkg_name=user-linux-api
 pkg_version=0.1.0
 pkg_origin=chef
 pkg_scaffolding="ci/scaffolding-chef-inspec"
-scaffold_inspec_client="chef/inspec/4.18.51"
+scaffold_inspec_client="chef/inspec/4.50.3"
